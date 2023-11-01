b52a9be9bd57e9e07f756579471fc21dac2ad68f
joaosant <137296210+russinhoeb@users.noreply.github.com>
2023-11-01 09:25:10 -0300

    2.2.js

--- VS2/2.2.js
+++ VS2/2.2.js
@@ -1,4 +1,4 @@
-const numero1 = prompt("Digite um algaritimo(NATURAL) para saber ele é par ou impar: ")
+let numero1 = prompt("Digite um algaritimo(NATURAL) para saber ele é par ou impar: ")
 if (numero1 % 2 === 0) {
     console.log("Par")
 } 
